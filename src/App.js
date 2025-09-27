ea861909761f697b063a07e2321af4fc6f86c408
hieuOneSt <hieuvh@arabicatech.vn>
2025-09-27 16:20:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "slick-carousel/slick/slick-theme.css";
 import SrcCodeBlog from "./SrcCodeBlog";
 
 function App() {
-  console.log("nani");
+  console.log("nani111");
+  console.log(222);
   
   return (
     <>
